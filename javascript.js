51899dd5e8cec402bda8966796775e8ddbf34db0
mikiyawv <mikiyawv@gmail.com>
2022-11-02 11:00:17 -0700

    Squares border shrinks as the # of squares goes past 44

--- javascript.js
+++ javascript.js
@@ -3,8 +3,8 @@ const range = document.querySelector('input');
 const p = document.querySelector('p');
 const reset = document.querySelector('.reset');
 
-board.style.gridTemplateColumns = `repeat(${range.value}, 1fr)`;
-board.style.gridTemplateRows = `repeat(${range.value}, 1fr)`;
+// board.style.gridTemplateColumns = `repeat(${range.value}, 1fr)`;
+// board.style.gridTemplateRows = `repeat(${range.value}, 1fr)`;
 
 const newBoard = function() {
     for (let r = 0; r < range.value * range.value; r++) {
@@ -15,8 +15,12 @@ const newBoard = function() {
         board.style.gridTemplateColumns = `repeat(${range.value}, 1fr)`;
         board.style.gridTemplateRows = `repeat(${range.value}, 1fr)`;
 
+        if (range.value >= 44) {
+            square.classList.add('smallSquare');
+        }
+
         square.addEventListener('mouseover', () => {
-        square.classList.add('active');
+            square.classList.add('active');
         });
     } //creates an initial 16x16 grid
 }
